82dde6577ca562c76a904c17ebfaf6c7db027ee8
Christophe Daloz - De Los Rios <christophedlr@gmail.com>
2022-06-25 17:22:23 +0200

    Adding commands
    - reboot Unix command
    - poweroff Unix command
    
    Signed-off-by: Christophedlr <christophedlr@gmail.com>

--- rmtm.py
+++ rmtm.py
@@ -2,13 +2,18 @@ import paramiko
 from scp import SCPClient
 
 class Rmtm:
-    ssh_connection: paramiko.SSHClient = paramiko.SSHClient();
+    ssh_connection: paramiko.SSHClient = paramiko.SSHClient()
 
     def open(self, host: str, user: str, password: str):
         self.ssh_connection.load_system_host_keys()
         self.ssh_connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        self.ssh_connection.connect(hostname=host, username=user, password=password);
+        self.ssh_connection.connect(hostname=host, username=user, password=password)
 
     def close(self):
         self.ssh_connection.close()
-    pass
+
+    def reboot_command(self):
+        self.ssh_connection.exec_command('/sbin/reboot')
+
+    def halt_command(self):
+        self.ssh_connection.exec_command('/sbin/poweroff')

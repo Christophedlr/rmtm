ad0104c42a3b1c17f4132934860751d82b706113
Christophe Daloz - De Los Rios <christophedlr@gmail.com>
2022-06-25 17:31:58 +0200

    Adding get file or files from ReMarkable
    Signed-off-by: Christophedlr <christophedlr@gmail.com>

--- rmtm.py
+++ rmtm.py
@@ -3,17 +3,23 @@ from scp import SCPClient
 
 class Rmtm:
     ssh_connection: paramiko.SSHClient = paramiko.SSHClient()
+    scp_connection: SCPClient
 
     def open(self, host: str, user: str, password: str):
         self.ssh_connection.load_system_host_keys()
         self.ssh_connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         self.ssh_connection.connect(hostname=host, username=user, password=password)
+        self.scp_connection = SCPClient(self.ssh_connection.get_transport())
 
     def close(self):
         self.ssh_connection.close()
+        self.scp_connection.close()
 
     def reboot_command(self):
         self.ssh_connection.exec_command('/sbin/reboot')
 
     def halt_command(self):
         self.ssh_connection.exec_command('/sbin/poweroff')
+
+    def get_file(self, remote, local, recursive: bool = False):
+        self.scp_connection.get(remote, local, recursive=recursive)

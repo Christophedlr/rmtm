3f0e7644cafb483c3745ae0f11e17ffae4302668
Christophe DALOZ - DE LOS RIOS <christophedlr@gmail.com>
2022-06-29 19:51:54 +0200

    Bug fix & mask password

--- console.py
+++ console.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import getpass
 import os.path
 
 import config
@@ -18,7 +19,7 @@ def choose_file(manager: Rmtm):
     if file_exists(file + ".png") and file_exists(file + ".svg"):
         name = input("Name: ")
 
-        templates.append(manager.add_entry(name, os.path.basename(file), ["Life/Organize"]))
+        templates.append(manager.add_entry(name, os.path.basename(file), ["Life/organize"]))
         filenames.append(file)
         choose_file(manager)
     elif file == "":
@@ -35,34 +36,35 @@ choose_file(manager)
 print()
 
 hostname = input("ReMarkable Hostname: ")
-password = input("ReMarkable root password: ")
+password = getpass.getpass("ReMarkable root password: ")
 template_json = input("Location to writing templates.json: ")
 
 print("Connecting to ReMarkable...")
 manager.open(hostname, "root", password)
 
 print("Downloading templates.json")
-manager.get_file("/usr/share/remarkable/templates/templates.json", template_json+"templates.json")
+manager.get_file("/usr/share/remarkable/templates/templates.json", template_json+"/templates.json")
 
 print("Parsing templates.json")
 
-with open(template_json+"templates.json", "r") as f:
+with open(template_json+"/templates.json", "r") as f:
     data = json.load(f)
 f.close()
 
-data["templates"].append(templates)
+for template in templates:
+    data["templates"].append(template)
 
-with open(template_json+"templates.json", "w") as f:
+with open(template_json+"/templates.json", "w") as f:
     json.dump(data, f, indent=2)
 f.close()
 
 for filename in filenames:
     print("Uploading " + filename)
-    manager.put_file(filename, "/usr/share/remarkable/templates/" + filename+".png")
-    manager.put_file(filename, "/usr/share/remarkable/templates/" + filename+".svg")
+    manager.put_file(filename+".png", "/usr/share/remarkable/templates/" + os.path.basename(filename)+".png")
+    manager.put_file(filename+".svg", "/usr/share/remarkable/templates/" + os.path.basename(filename)+".svg")
 
 print("Uploading templates.json")
-manager.put_file(template_json+"templates.json", "/usr/share/remarkable/templates/templates.json")
+manager.put_file(template_json+"/templates.json", "/usr/share/remarkable/templates/templates.json")
 
 print("Rebooting ReMarkable")
 manager.reboot_command()
